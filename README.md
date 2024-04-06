8055ce67345b5a454729164e27b27612c13a8923
Jacob Adelman <jacobzadelman@gmail.com>
2024-04-06 09:08:31 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# TikTok Search Api
+
+## How to Install
+
+```
+npm install tiktok-search-api
+```
+
+## How to Use
+
+```
+import { TikTokSearch } from 'tiktok-search-api'
+
+const results = TikTokSearch(keyword, ttwid, pageCount)
+```
+
+## Arguments
+
+- **keyword (String)**: This is the keyword you would like to search for (uses TikTok search)
+- **ttwid (String)**: ttwid cookie
+  - get this from your Application--cookies tab in your browser
+  - include only the cookie with no semicolon, equals sign or cookie name
+- **pageCount (Number)**: number of pages you would like to get
+  - each page is ~12 videos, so pageCount of 10 would give ~120 results
+
+## Results
+
+Here is a sample of the results. The results are an array of video objects with the most important parts being the item.id (video id) and the author object where you can find the author uniqueId among other things
+
+```
+ [{
+    type: 1,
+    item: {
+      id: '7202140515909782790',
+      desc: 'Retraite 3days 🤍🙌🏾#fypシ #christiantiktok #God #retraitespirituelle ',
+      createTime: 1676879023,
+      video: [Object],
+      author: [Object],
+      music: [Object],
+      challenges: [Array],
+      stats: [Object],
+      duetInfo: [Object],
+      originalItem: false,
+      officalItem: false,
+      textExtra: [Array],
+      secret: false,
+      forFriend: false,
+      digged: false,
+      itemCommentStatus: 0,
+      showNotPass: false,
+      vl1: false,
+      itemMute: false,
+      authorStats: [Object],
+      privateItem: false,
+      duetEnabled: true,
+      stitchEnabled: true,
+      shareEnabled: true,
+      stickersOnItem: [Array],
+      isAd: false,
+      collected: false
+    },
+    common: { doc_id_str: '7202140515909782790' }
+  },
+  ...
+ ]
+```
+
+## Errors
+
+Not tested with high page count. It is recommended to start off with a pageCount of 3 - 7. Using higher page counts may give errors.
---
+++ src/index.js
@@ -0,0 +1,80 @@
+async function fetchTikTokSearch(keyword, ttwid) {
+  return new Promise(async (resolve) => {
+    let cookieString = "ttwid=" + ttwid + ";";
+    let urlString = `https://www.tiktok.com/api/search/general/full/?`;
+    let ops = [
+      { name: "from_page", val: "search" },
+      { name: "keyword", val: keyword },
+    ];
+
+    for (let b = 0; b < ops.length; b++) {
+      urlString = urlString + ops[b].name + "=" + ops[b].val + "&";
+    }
+
+    fetch(urlString, {
+      method: "GET",
+      headers: {
+        Cookie: cookieString,
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        resolve(data.data);
+      });
+  });
+}
+async function fetchTikTokSearch2(keyword, offset, searchId, ttwid) {
+  return new Promise(async (resolve) => {
+    let cookieString = "ttwid=" + ttwid + ";";
+    let urlString = `https://www.tiktok.com/api/search/general/full/?`;
+    let ops = [
+      { name: "from_page", val: "search" },
+      { name: "keyword", val: keyword },
+      { name: "offset", val: offset },
+      { name: "search_id", val: searchId },
+    ];
+
+    for (let b = 0; b < ops.length; b++) {
+      urlString = urlString + ops[b].name + "=" + ops[b].val + "&";
+    }
+
+    fetch(urlString, {
+      method: "GET",
+      headers: {
+        Cookie: cookieString,
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        resolve(data.data);
+      });
+  });
+}
+async function TikTokSearch(keyword, ttwid, pages) {
+  return new Promise(async (resolve) => {
+    let search1 = await fetchTikTokSearch("hello", ttwid);
+    let searchId = search1[0].common.doc_id_str;
+    let finalArr = [];
+    for (let i = 1; i < pages; i++) {
+      let iPlus = i + 1;
+      let myArr = await fetchTikTokSearch2(
+        keyword,
+        iPlus * 12,
+        searchId,
+        ttwid
+      );
+      myArr.map((arrItem) => {
+        finalArr.push(arrItem);
+      });
+      if (i == pages - 1) {
+        resolve(finalArr);
+      }
+    }
+  });
+}
+
+module.exports = { TikTokSearch };
